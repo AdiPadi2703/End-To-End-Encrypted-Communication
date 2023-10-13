688e3fafc585415ba57ad57a1aa5d96c2813bf84
Adithya Ubaradka <122197400+AdiPadi2703@users.noreply.github.com>
2023-10-13 23:28:39 +0530

    Final code and logisim

--- test_bench.v
+++ test_bench.v
@@ -1,32 +1,39 @@
 module testbench;
 
-    reg [0:3] p;
-    reg [0:3] q;
-    reg [0:3] p_;
-    reg [0:3] q_;
-    wire [0:7] n;
-    wire [0:7] phi;
+    reg [24:0] p;
+    reg [24:0] q;
+    reg [24:0] p_;
+    reg [24:0] q_;
+    reg [24:0] letter;
+    wire [24:0] n;
+    reg [24:0] e;
+    reg [24:0] d;
+    wire [24:0] phi;
+    wire [24:0] en_letter;
+    wire [24:0] de_letter;
 
-    fourbitmultiplier m1(p[0],p[1],p[2],p[3],q[0],q[1],q[2],q[3],n[0],n[1],n[2],n[3],n[4],n[5],n[6],n[7]);
-    fourbitmultiplier m2(p_[0],p_[1],p_[2],p_[3],q_[0],q_[1],q_[2],q_[3],phi[0],phi[1],phi[2],phi[3],phi[4],phi[5],phi[6],phi[7]);
+    sixteenbitmultiplier m1(p,q,n);
+    sixteenbitmultiplier m2(p-24'd1,q-24'd1,phi);
+    encryptor en(e,n,letter,en_letter);
+    decryptor de(d,n,en_letter,letter,de_letter);
 
-initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars(0);
-end
+    initial begin
+        $dumpfile("tb.vcd");
+        $dumpvars(0);
+    end
 
-initial begin
-    p = 4'b0011; //3
-    q = 4'b0111; //7
-    p_ = p - 4'b0001;
-    q_ = q - 4'b0001;
-    $display("a[0]  a[1]  a[2]  a[3]    b[0]  b[1]  b[2]  b[3]   p[0]  p[1]  p[2]  p[3]  p[4]  p[5]  p[6]  p[7]");
-    $monitor(" %b   %b    %b     %b      %b    %b    %b    %b    %b     %b    %b    %b    %b    %b    %b    %b  ",p[0],p[1],p[2],p[3],q[0],q[1],q[2],q[3],n[0],n[1],n[2],n[3],n[4],n[5],n[6],n[7]);
-    #10
-    $monitor(" %b   %b    %b     %b      %b    %b    %b    %b    %b     %b    %b    %b    %b    %b    %b    %b  ",p_[0],p_[1],p_[2],p_[3],p_[0],q_[1],q_[2],q_[3],phi[0],phi[1],phi[2],phi[3],phi[4],phi[5],phi[6],phi[7]);
+    initial begin
+       p =24'd3;
+       q =24'd7;
+       e =24'd5;
+       d =24'd5;
+       $display("Input letter number (a = 1)      Decrypted after encryption");
+       $display("-----------------------------------------------------------");
+       for(letter =24'd1;letter <24'd27;letter = letter +24'd1)begin
+            $display("          %d                 %d",letter,de_letter);
+       end
+    end
 
-end
-
-initial #400 $finish;
+    initial #400 $finish;
 
 endmodule
