321a79d3fea65075a3f4ba282d8b3562dd34907a
Kazedaa <120291477+Kazedaa@users.noreply.github.com>
2023-11-02 20:33:52 +0530

    Add files via upload

--- Verilog/modules.v
+++ Verilog/modules.v
@@ -4,6 +4,20 @@ module sixteenbitmultiplier(input[25:0] p,q,output[25:0] n);
 
 endmodule
 
+module keyGenerator(input clk,input[25:0] phi,output[25:0] e,d);	
+	reg[25:0] i,p;
+	initial begin
+		i=26'd2;
+		p=26'd24;
+		while(p%i==0)begin
+			i=i+26'd1;
+		end
+	end
+	assign e=i;
+	assign d=(phi+26'd1)/i;
+
+endmodule
+
 module encryptor(input[25:0] e,n,letter, output[25:0] en_letter);
 
     assign en_letter = (letter**e)%n;
